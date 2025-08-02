08abf6ba0ee8ce7b36fa9faf4780963da6dd9d20
Sahmey Raiyan Khan <sahmeykhan88@gmail.com>
2025-08-03 03:17:28 +0600

    Updated App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AuthProvider } from '@/hooks/useAuth'
 import { Toaster } from '@/components/ui/toaster'
-import Index from '@/pages/Index'
-import Auth from '@/pages/Auth'
-import UpdatePassword from '@/pages/UpdatePassword'
-import NotFound from '@/pages/NotFound'
-import ProtectedRoute from '@/components/ProtectedRoute'
+import App from './App.tsx'
+import './index.css'
 
 const queryClient = new QueryClient()
 
@@ -17,19 +14,7 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <BrowserRouter>
-          <Routes>
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/update-password" element={<UpdatePassword />} />
-            <Route 
-              path="/" 
-              element={
-                <ProtectedRoute>
-                  <Index />
-                </ProtectedRoute>
-              } 
-            />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <App />
           <Toaster />
         </BrowserRouter>
       </AuthProvider>
